f6f577dd854481626013c84013f9db7d9c2b24de
Ifeyibatindek <ifeyibatindek@gmail.com>
2025-01-29 13:22:57 +0100

    User part done

--- helpdeskassets/helpdeskscripts/helpdeskmain.js
+++ helpdeskassets/helpdeskscripts/helpdeskmain.js
@@ -43,6 +43,7 @@ if(helpdeskAccordionElt){
 let helpdeskBtnQuitter = document.getElementById("helpdesk-btn-quitter");
 let helpdeskDemandeDeincidentSingle = document.getElementById("helpdesk-demande-de-incident-single");
 let helpdeskTableauincidentBody = document.querySelectorAll(".helpdesk-tableau-incident-body");
+let helpdeskIncidentSingleSubmit = document.getElementById("helpdesk-incident-single-submit");
 
 helpdeskTableauincidentBody.forEach((elt) => {
     elt.addEventListener("click", () => {
@@ -54,5 +55,77 @@ if(helpdeskBtnQuitter){
     helpdeskBtnQuitter.addEventListener("click", () => {
         helpdeskDemandeDeincidentSingle.classList.toggle("helpdesk-none");
     });
+    helpdeskIncidentSingleSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        helpdeskDemandeDeincidentSingle.classList.toggle("helpdesk-none");
+    });
 }
 
+/* Déclaration incident */
+
+let helpdeskDeclarerIncident = document.getElementById("helpdesk-declarer-incident");
+let helpdeskIncidentFormWrapper = document.getElementById("helpdesk-incident-form-wrapper");
+let helpdeskIncidentFormSvgAnnuler = document.getElementById("helpdesk-incident-form-svg-annuler");
+let helpdeskIncidentFormSubmit = document.getElementById("helpdesk-incident-form-submit");
+let helpdeskIncidentFormError = document.getElementById("helpdesk-incident-form-error");
+let helpdeskIncidentFormTitle = document.getElementById("helpdesk-incident-form-title");
+let helpdeskIncidentFormDescription = document.getElementById("helpdesk-incident-form-description");
+let helpdeskIncidentFormInputs = document.querySelectorAll(".helpdesk-incident-form .helpdesk-input");
+
+if(helpdeskDeclarerIncident){
+    helpdeskDeclarerIncident.addEventListener("click", () => {
+        helpdeskIncidentFormWrapper.classList.toggle("helpdesk-none");
+    });
+    helpdeskIncidentFormSvgAnnuler.addEventListener("click", () => {
+        helpdeskIncidentFormWrapper.classList.toggle("helpdesk-none");
+    });
+    helpdeskIncidentFormSubmit.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(helpdeskIncidentFormTitle.value === "" || helpdeskIncidentFormDescription.value === ""){
+            helpdeskIncidentFormError.innerHTML = "<p class='helpdesk-error-text'>Veuillez donner un titre et une description à votre incident.</p>";
+        }
+    });
+    helpdeskIncidentFormInputs.forEach(helpdeskIncidentFormInput => helpdeskIncidentFormInput.addEventListener("input", () => {
+        if(helpdeskIncidentFormInput.value !== ""){
+            helpdeskIncidentFormError.innerHTML = "";
+        }
+    }));
+
+}
+/* Déclaration incident */
+
+/* Commentaires incident */
+let helpdeskComBtnAjouter = document.getElementById("helpdesk-com-btn-ajouter");
+let helpdeskComBtnAnnuler = document.getElementById("helpdesk-com-btn-annuler");
+let helpdeskComBtnEnvoyer = document.getElementById("helpdesk-com-btn-envoyer");
+let helpdeskComForm = document.getElementById("helpdesk-com-form");
+let helpdeskComText = document.getElementById("helpdesk-com-text");
+let helpdeskComFormError = document.getElementById("helpdesk-com-form-error");
+
+if(helpdeskComBtnAjouter){
+    helpdeskComBtnAjouter.addEventListener("click", () => {
+
+        helpdeskComForm.classList.toggle("helpdesk-none");
+        helpdeskComBtnAjouter.classList.toggle("helpdesk-none");
+    });
+    helpdeskComBtnAnnuler.addEventListener("click", (e) => {
+        e.preventDefault();
+        helpdeskComForm.classList.toggle("helpdesk-none");
+        helpdeskComBtnAjouter.classList.toggle("helpdesk-none");
+    });
+    helpdeskComBtnEnvoyer.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(helpdeskComText.value === ""){
+            helpdeskComFormError.innerHTML = "<p class='helpdesk-error-text'>Vous ne pouvez pas envoyer un commentaire vide.</p>"
+        } else{
+            helpdeskComForm.classList.toggle("helpdesk-none");
+            helpdeskComBtnAjouter.classList.toggle("helpdesk-none");
+        }
+    });
+    helpdeskComText.addEventListener("input", () => {
+        if(helpdeskComText.value !== ""){
+            helpdeskComFormError.innerHTML = "";
+        }
+    });
+}
+/* Commentaires incident */
