b089854983818e9bb6fc4c8cce0f9fcf3cf9f864
Ifeyibatindek <ifeyibatindek@gmail.com>
2025-01-29 15:37:46 +0100

    Python files

--- cfcHelpdesk/cfcHelpdesk/urls.py
+++ cfcHelpdesk/cfcHelpdesk/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')), # This line tells Django to include the URLs from the core application.
 ]
--- cfcHelpdesk/core/views.py
+++ cfcHelpdesk/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html') # This line is the only one that is different from the previous snippet. It tells Django to render the index.html file when the user accesses the home page.
+
+def login(request):
+    return render(request, 'login.html') # This line tells Django to render the login.html file when the user accesses the login page.
